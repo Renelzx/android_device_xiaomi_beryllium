46a9e6319f3be0272ec74f7f44128a6baf0368c0
Renelz <131983767+Renelzx@users.noreply.github.com>
2026-05-08 03:40:46 +0700

    Update kasumi_beryllium.mk with new configurations

--- kasumi_beryllium.mk
+++ kasumi_beryllium.mk
@@ -9,8 +9,21 @@ $(call inherit-product, device/xiaomi/beryllium/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Kasumi
+KASUMI_SHIP_LAWNCHAIR := true
+KASUMI_SHIP_ADAWAY := true
+KASUMI_INCLUDE_GCGOP := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# GApps
+KASUMI_BUILD_TYPE := gapps
+TARGET_GAPPS_ARCH := arm64
+
+# Bootanimation
+TARGET_BOOT_ANIMATION_RES := 1080
+
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_beryllium
+PRODUCT_NAME := kasumi_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO F1
